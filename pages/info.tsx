1b4918b4013630a2f5381e4161ec240ec495621d
ryanlei2 <ryanlei726@gmail.com>
2024-02-28 21:00:32 -0800

    test info

--- pages/info.tsx
+++ pages/info.tsx
@@ -22,6 +22,7 @@ const Faq = () => {  return (
       </div>
       <div className={styles.faq}>
         <title>What is Green Energy?</title>
+        <h3>What is Green Energy</h3>
         Green energy refers to energy generated through methods that preserve the natural environment, which can wind, water, or solar, or other various forms of natural energy sources.
         <br/><br/>
         Green energy is harnessed from renewable sources that do not deplete over time and do not emit pollutants during production, unlike fossil fuels; however, it&apos;s important to note that not all energy sources labeled as renewable are inherently green. For instance, while power generation from sustainable forest biomass is renewable, the CO2 emissions from the combustion process make it less environmentally friendly. Green energy stands out as the most eco-friendly resource available today, with minimal ongoing environmental impact, but it&apos;s up to <strong>you</strong> as home owners to take control of its power.
@@ -41,7 +42,10 @@ const Faq = () => {  return (
               fontStyle: 'italic'
             }}>See how each practice has benefits and drawbacks?</figcaption>
         </figure>
-      
+        <br/><br/><br/><br/>
+        <title>Why Go Green?</title>
+        <h3>Why Go Green?</h3>
+        Customers often buy green power for its zero-emissions profile and carbon footprint reduction benefits. If they switch, they can potentially save money and 
         <HexagonButton/>
       </div>
     </Container>
